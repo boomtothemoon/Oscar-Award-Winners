344652ff7fa88dba3832439e0d22ac684556ee1c
Christian Carlo Cruz <christiancruz.carlo@gmail.com>
2018-02-16 04:02:51 +0000

    Added relationship between Award, AwardWinner and Winner table

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,3 +1,5 @@
 class Award < ApplicationRecord
+    has_many :AwardWinner
+    has_many :Winner, through: :AwardWinner
     validates :year, presence: true
 end
--- app/models/winner.rb
+++ app/models/winner.rb
@@ -1,3 +1,5 @@
 class Winner < ApplicationRecord
+    has_many :AwardWinner
+    has_many :Award, through: :AwardWinner
     validates :name, :age, presence: true
 end
